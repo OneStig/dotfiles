68bc2cfd129590d69e6be8614bfb317f6f88c8b9
Steven He <hess1@uci.edu>
2024-12-27 12:01:24 -0800

    revised cpp template

---
+++ cp/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "cpp-dump/cpp-dump.hpp"
+#define dbg(...) cpp_dump(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef int uci;
+#define int long long
+#define ld long double
+#define ve vector
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef pair<int, int> pii;
+
+uci main() {
+    cin.tie(0)->sync_with_stdio(0);
+}
